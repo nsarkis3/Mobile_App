0469ee7f62149d66ed44765fec93f283f0f1e278
Nic Sarkis <sarkisn@miamioh.edu>
2023-08-30 10:34:15 -0400

    Created Maui App.

---
+++ MyFirst/MyFirst/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirst;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyFirst/MyFirst/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyFirst;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
