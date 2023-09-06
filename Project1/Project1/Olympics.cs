9f1c196eef9b6f897aad31c86c333af359bcba61
Nic Sarkis <sarkisn@miamioh.edu>
2023-09-06 11:38:37 -0400

    Starter Code

---
+++ Project1/Project1/Olympics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1;
+public class Olympics {
+	public static List<Participant>? participants;
+
+	public static List<Participant> ReadParticipants(string fname) {
+		participants = new List<Participant>();
+		using (StreamReader input = new StreamReader(fname)) {
+			string? header = input.ReadLine();
+			while (!input.EndOfStream) {
+				string? line = input.ReadLine();
+				if (line == null) {
+					continue;
+				}
+				Participant p = new Participant(line);
+				participants.Add(p);
+			}
+		}
+		return participants;
+	}
+	public static void Main(string[] args) {
+		ReadParticipants("olympics.tsv");
+
+		while (true) {
+			Console.Write(">> ");
+			string? userInput = Console.ReadLine();
+		}
+	}
+}
---
+++ Project1/Project1/Participant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1;
+public class Participant {
+	public enum MedalType { None, Bronze, Silver, Gold };
+	public enum SeasonType { Summer, Winter };
+	public enum GenderType { Female, Male };
+
+	public int ID { get; set; }
+	public String Name { get; set; }
+	public GenderType Gender { get; set; }
+	public int Age { get; set; }
+	public String Country { get; set; }
+	public int Year { get; set; }
+	public SeasonType Season { get; set; }
+	public String Location { get; set; }
+	public String Sport { get; set; }
+	public String Event { get; set; }
+	public MedalType Medal { get; set; }
+
+	public Participant(String str) {
+		string[] toks = str.Split(new char[] { '\t' });
+	}
+}
